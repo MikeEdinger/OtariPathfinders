eeb4316ae3e168fc3203738bb2d0498ac859da65
MikeEdinger <mikeedinger16@gmail.com>
2024-02-02 12:06:00 -0500

    Quartz sync: Feb 2, 2024, 12:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "Abomination Vaults\Session Plans"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Abomination Vaults/Session Plans"],
     defaultDateType: "created",
     theme: {
       typography: {
